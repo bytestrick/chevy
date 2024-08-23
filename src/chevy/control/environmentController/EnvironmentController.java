018c5b7e24194a3dd933a29b0b078d373379b6d5
Carlino Giorgio <carlinogiorgiogithub@gmail.com>
2024-08-23 22:15:29 +0200

    Abbozzo cambio stanza (da migliorare)

--- src/chevy/control/environmentController/EnvironmentController.java
+++ src/chevy/control/environmentController/EnvironmentController.java
@@ -1,9 +1,11 @@
 package chevy.control.environmentController;
 
+import chevy.control.ChamberController;
 import chevy.control.HUDController;
 import chevy.control.InteractionType;
 import chevy.model.chamber.Chamber;
 import chevy.model.entity.Entity;
+import chevy.model.entity.dinamicEntity.liveEntity.player.Player;
 import chevy.model.entity.staticEntity.environment.Chest;
 import chevy.model.entity.staticEntity.environment.Environment;
 import chevy.model.entity.staticEntity.environment.Stair;
@@ -12,16 +14,22 @@ public class EnvironmentController {
     private final ChestController chestController;
     private final StairController stairController;
 
-    public EnvironmentController(Chamber chamber, HUDController hudController) {
+    public EnvironmentController(Chamber chamber, HUDController hudController, ChamberController chamberController) {
         this.chestController = new ChestController(chamber, hudController);
-        this.stairController = new StairController(chamber);
+        this.stairController = new StairController(chamber, chamberController);
     }
 
 
     public synchronized void handleInteraction(InteractionType interaction, Entity subject, Entity object) {
         switch (interaction) {
+            case PLAYER_IN -> playerInInteraction((Player) subject, (Environment) object);
             case UPDATE -> updateTraps((Environment) subject);
         }
+    }
+
+    private void playerInInteraction(Player subject, Environment environment) {
+        if (environment.getSpecificType() == Environment.Type.STAIR)
+            stairController.playerInInteraction((Stair) environment);
     }
 
     private void updateTraps(Environment environment) {
--- src/chevy/control/environmentController/StairController.java
+++ src/chevy/control/environmentController/StairController.java
@@ -1,13 +1,22 @@
 package chevy.control.environmentController;
 
+import chevy.control.ChamberController;
 import chevy.model.chamber.Chamber;
 import chevy.model.entity.staticEntity.environment.Stair;
 
 public class StairController {
     private final Chamber chamber;
+    private final ChamberController chamberController;
 
-    public StairController(Chamber chamber) {
+    public StairController(Chamber chamber, ChamberController chamberController) {
         this.chamber = chamber;
+        this.chamberController = chamberController;
+    }
+
+
+    public void playerInInteraction(Stair stair) {
+        if (stair.getCurrentState() == Stair.State.IDLE_ENTRY)
+            chamberController.changeNextChamber();
     }
 
     public void update(Stair stair) {
