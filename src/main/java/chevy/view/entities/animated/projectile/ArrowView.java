97d18abc3a5a17d1fdebe4139187ce71b102f26a
Mario D'Andrea <goffredo2004@gmail.com>
2024-10-27 13:26:55 +0100

    fix: rimosso doppi / nel percorso

--- src/main/java/chevy/view/entities/animated/projectile/ArrowView.java
+++ src/main/java/chevy/view/entities/animated/projectile/ArrowView.java
@@ -12,7 +12,7 @@ public final class ArrowView extends ProjectileView {
         final float endDuration = projectile.getState(Arrow.State.END).getDuration();
         for (Direction direction : Direction.values()) {
             final String dir = direction.toString().toLowerCase();
-            animate(Arrow.State.LOOP, direction, 4, loopDuration, res + "/loop/" + dir);
+            animate(Arrow.State.LOOP, direction, 4, loopDuration, res + "loop/" + dir);
             animate(Arrow.State.END, direction, 4, endDuration, res + "end");
         }
     }
